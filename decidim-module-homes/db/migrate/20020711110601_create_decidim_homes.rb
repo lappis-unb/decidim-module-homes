204b7821948fbc662721ee3aeeaf066d5a2eadea
Giovanni Alvissus <giovanni.acg@gmail.com>
2023-12-05 11:32:50 -0300

    feat: atualizando migrates

--- decidim-module-homes/db/migrate/20020711110601_create_decidim_homes.rb
+++ decidim-module-homes/db/migrate/20020711110601_create_decidim_homes.rb
@@ -5,13 +5,20 @@ class CreateDecidimHomes < ActiveRecord::Migration[5.0]
     create_table :decidim_homes_homes do |t|
       t.jsonb :title
       t.references :decidim_feature, index: true
+      t.string :banner
+      t.string :digital_stage, default: '/'
+      t.string :organize_stage, default: '/'
+      t.string :schedule, default: '/'
+      t.string :common_questions, default: '/'
+      t.string :support_material, default: '/'
       t.boolean :statistics, default: false
       t.boolean :news, default: false
       t.integer :news_id
-      t.string :banner
       t.jsonb :organizers, default: []
+      t.jsonb :supporters, default: []
 
       t.timestamps
     end
   end
 end
+
