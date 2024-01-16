1a7f938937b1c4a53758d3cdd607a98457c14654
Giovanni Alvissus <giovanni.acg@gmail.com>
2024-01-15 21:05:35 -0300

    fix: alterando nome de arquivo

--- app/uploaders/decidim/homes/image_uploader.rb
+++ app/uploaders/decidim/homes/image_uploader.rb
@@ -2,9 +2,13 @@ module Decidim
     module Homes
         class ImageUploader < CarrierWave::Uploader::Base
             include CarrierWave::MiniMagick
-        
+            
             storage :file
-        
+            
+            def validate_integrity
+            errors.add(:file, 'cannot be empty') if file.blank?                      
+            end
+
             def store_dir
             "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
             end
